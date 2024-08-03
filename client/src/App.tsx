31b37a3d68577e1c4944a0fe8603a84357f0185c
Maria <salyginamaria156@gmail.com>
2024-08-03 21:24:38 +0300

    add: init client

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import s from "./App.module.scss"
+
+function App() {
+
+  return (
+    <>
+    <div className={s.box}>
+      <p className={s.text}>fsdfsdf</p>
+    </div>
+    </>
+  )
+}
+
+export default App
